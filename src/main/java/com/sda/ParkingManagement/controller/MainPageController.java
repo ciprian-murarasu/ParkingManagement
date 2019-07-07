e901afa1a6c7215f283446c82b8e8a99ec6907d2
ciprian-murarasu <cypris_84@yahoo.com>
2019-07-07 16:05:19 +0300

    added jsp

--- src/main/java/com/sda/ParkingManagement/controller/MainPageController.java
+++ src/main/java/com/sda/ParkingManagement/controller/MainPageController.java
@@ -1,7 +1,6 @@
 package com.sda.ParkingManagement.controller;
 
 import com.sda.ParkingManagement.model.ParkingSpace;
-import com.sda.ParkingManagement.model.Sector;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class ParkingSpaceController {
+public class MainPageController {
 
     private static List<ParkingSpace> parkingSpaces = new ArrayList<ParkingSpace>();
-
-//    static {
-//        parkingSpaces.add(new ParkingSpace(12, new Sector("A", new ));
-//    }
 
     @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
