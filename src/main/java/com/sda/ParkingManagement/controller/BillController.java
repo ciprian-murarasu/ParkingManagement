87617e6dc84c91a2966149dde1d76644b0117f41
=Andrei <=balasca.andrei@gmail.com>
2019-07-13 17:42:23 +0300

    started pay bill+/payBill+

--- src/main/java/com/sda/ParkingManagement/controller/BillController.java
+++ src/main/java/com/sda/ParkingManagement/controller/BillController.java
@@ -1,10 +1,19 @@
 package com.sda.ParkingManagement.controller;
 
+import com.sda.ParkingManagement.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/payBill")
 public class BillController {
+
+    private TicketService ticketService;
+
+    @Autowired
+    public BillController(TicketService ticketService){
+        this.ticketService = ticketService;
+    }
     
 }
