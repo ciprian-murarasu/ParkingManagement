635a040409e9828039b8b2fbe08d8000ef189ab0
=Andrei <=balasca.andrei@gmail.com>
2019-07-14 11:55:28 +0300

    started pay bill+/payBill1

--- src/main/java/com/sda/ParkingManagement/service/BillService.java
+++ src/main/java/com/sda/ParkingManagement/service/BillService.java
@@ -1,5 +1,15 @@
 package com.sda.ParkingManagement.service;
 
+import com.sda.ParkingManagement.repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BillService {
+    private TicketRepository ticketRepository;
+
+    public BillService(TicketRepository ticketRepository){
+        this.ticketRepository = ticketRepository;
+    }
+
 
 }
