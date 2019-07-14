e995ce75be9348644e5bbdf3b511c11f67c09d08
=Andrei <=balasca.andrei@gmail.com>
2019-07-14 11:16:30 +0300

    started pay bill+/payBill1

--- src/main/java/com/sda/ParkingManagement/controller/BillController.java
+++ src/main/java/com/sda/ParkingManagement/controller/BillController.java
@@ -1,8 +1,13 @@
 package com.sda.ParkingManagement.controller;
 
+import com.sda.ParkingManagement.dto.TicketDto;
 import com.sda.ParkingManagement.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +19,18 @@ public class BillController {
     @Autowired
     public BillController(TicketService ticketService){
         this.ticketService = ticketService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String payBill(TicketDto ticketDto, Model model){
+        String billCode = ticketDto.getCode();
+        if(StringUtils.isEmpty(billCode)){
+            model.addAttribute("billMessage","Please enter a Ticket code");
+        }
+        else {
+            
+        }
+        return "index";
     }
     
 }
--- src/main/java/com/sda/ParkingManagement/service/BillService.java
+++ src/main/java/com/sda/ParkingManagement/service/BillService.java
@@ -1,4 +1,5 @@
 package com.sda.ParkingManagement.service;
 
 public class BillService {
+
 }
